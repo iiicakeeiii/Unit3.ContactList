a09b90b9539c3398c2b772a749713145affb8750
Marc Charles <marccharles243@gmail.com>
2023-12-20 21:22:56 -0500

    contact list project

---
+++ contact-list-project/src/componets/ContactRow.jsx
@@ -0,0 +1,17 @@
+import "./ContactList.jsx"
+
+const ContactRow = ({contact}) => {
+    return(
+        <>
+            <div>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </div>
+        </>
+    )
+}
+
+export default ContactRow;
